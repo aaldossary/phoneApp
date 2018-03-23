e0143dac4d6e580cd28396709549d271fe5fb7eb
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-03-22 21:03:34 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$('document').ready(function(){
+
+  $('#popupM').fadeOut(3000); 
+
+  
+  });
+                    
+
+
+function myFunction() {
+    var signedInM = document.getElementById("welcomeM");
+   signedInM.classList.toggle("show");
+   
+ 
+  
+}
